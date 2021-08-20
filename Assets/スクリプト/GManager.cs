417bd8b909e017b2f84cfeeaecfa101bcaee3ce7
DESKTOP-IAEPM7R\vantan <04022005rikuriku@gmail.com>
2021-08-20 16:32:28 +0900

    針に当たってdownするscriptですがうまくいかないので修正します

--- Assets/スクリプト/GManager.cs
+++ Assets/スクリプト/GManager.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class GManager : MonoBehaviour
 {
     public static GManager instance = null;
-    public int score;
-    public int stageNum;
-    public int continueNum;
-    public int heartNum;
     
+    [Header("スコア")]public int score;
+    [Header("現在のステージ")] public int stageNum;
+    [Header("現在の復帰位置")] public int continueNum;
+    [Header("現在の残機")] public int heartNum;
+    [Header("デフォルトの残機")] public int defaultHeartNum;
+    [HideInInspector] public bool isGameOver;
+
     private void Awake()
     {
         if(instance==null)
@@ -20,6 +23,30 @@ public class GManager : MonoBehaviour
         else
         {
             Destroy(this.gameObject);
+        }
+    }
+    /// <summary>
+    /// 残機を一つ増やす
+    /// </summary>
+    public void AddHeartNum()
+    {
+        if(heartNum<99)
+        {
+            ++heartNum;
+        }
+    }
+    /// <summary>
+    /// 残機を一つ減らす
+    /// </summary>
+    public void SubHeartNum()
+    {
+        if(heartNum>0)
+        {
+            --heartNum;
+        }
+        else
+        {
+            isGameOver = true;
         }
     }
 }
