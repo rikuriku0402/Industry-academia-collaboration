9210fd946caf27a9c771040c0861bf11586b8e34
DESKTOP-IAEPM7R\vantan <04022005rikuriku@gmail.com>
2021-10-13 21:49:33 +0900

    アニメーションやステージを追加した

---
+++ Assets/スクリプト/ステージ2のscript/rotationYuka.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationYuka : MonoBehaviour
+{
+    //[Header("ローテーション")] public float RotAngle = 4.0f;
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(0, 0, 5));
+    }
+}
