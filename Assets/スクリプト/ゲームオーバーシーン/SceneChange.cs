4a78e4ae25ff1c9a448f8595debef1fd543afd11
DESKTOP-IAEPM7R\vantan <04022005rikuriku@gmail.com>
2021-10-19 22:26:53 +0900

    色んなsceneの背景を変えたりしてた

---
+++ Assets/スクリプト/ゲームオーバーシーン/SceneChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Retrybutton()
+    {
+        SceneManager.LoadScene("titleScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
